4087b4d9bc89ba54c947b1773e5ca699f45e1c0e
Fabio Muriel <fabiomurielvega16@gmail.com>
2024-01-09 17:47:05 -0500

    Update AssemblyInfo with version information

--- obj/Debug/net6.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net6.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6085f20f033339ced0fd09bfa7f32008cf5ba3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
